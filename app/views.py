10e32ca0fd3b27c1ddb141722bf74e4dd9669f7e
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2020-09-02 17:49:21 +0200

    result forms, missing the computation

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from .forms import PreferencesForm
 
 app = Flask(__name__)
@@ -12,5 +12,6 @@ from .models import Car
 def contact():
     form = PreferencesForm()
     if form.validate_on_submit():
-        return redirect(url_for('success'))
+        print(form.fuel_type.data)
+        return render_template('results.html')
     return render_template('preferences.html', form=form)
